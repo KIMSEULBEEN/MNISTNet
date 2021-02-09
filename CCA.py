6fb84a0b6c299f8ed694c4dedc38871f492f3577
Seulbeen Kim <tmfqksdk@gmail.com>
2021-02-09 17:25:25 +0900

    AdaptiveThreshold 블럭 사이즈 변경

--- CCA.py
+++ CCA.py
@@ -1,12 +1,16 @@
 import cv2
 
-image = cv2.imread('sample2.jpg', 0)
-image_bin = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 49, 2)
+image = cv2.imread('sample.jpg', 0)
+_, width = image.shape
+block_size = width // 5 if (width // 5) % 2 == 1 else width // 5 + 1
+print("block size: ", block_size)
+
+image_bin = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 151, 30)
 image_bin = cv2.bitwise_not(image_bin)
 cnt, labels, stats, centroids = cv2.connectedComponentsWithStats(image_bin)
 
 
-for i in range(1, cnt): 
+for i in range(1, cnt):
     (x, y, w, h, area) = stats[i]
     if area < 50: continue
 
