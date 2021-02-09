660fea723ad5094c39f7d8c2d6360b4404cd0e9e
Seulbeen Kim <tmfqksdk@gmail.com>
2021-02-09 17:17:56 +0900

    주석 제거

--- CCA.py
+++ CCA.py
@@ -6,7 +6,7 @@ image_bin = cv2.bitwise_not(image_bin)
 cnt, labels, stats, centroids = cv2.connectedComponentsWithStats(image_bin)
 
 
-for i in range(1, cnt): # 각각의 객체 정보에 들어가기 위해 반복문. 범위를 1부터 시작한 이유는 배경을 제외
+for i in range(1, cnt): 
     (x, y, w, h, area) = stats[i]
     if area < 50: continue
 
