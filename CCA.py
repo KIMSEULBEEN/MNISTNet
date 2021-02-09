0747e482328af7413432097137b12486349348c9
Seulbeen Kim <tmfqksdk@gmail.com>
2021-02-09 17:17:39 +0900

    코드 정리 + AdaptiveThreshold 추가

--- CCA.py
+++ CCA.py
@@ -1,24 +1,18 @@
 import cv2
 
-src = cv2.imread('images.png', 0)
+image = cv2.imread('sample2.jpg', 0)
+image_bin = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 49, 2)
+image_bin = cv2.bitwise_not(image_bin)
+cnt, labels, stats, centroids = cv2.connectedComponentsWithStats(image_bin)
 
-_, src_bin = cv2.threshold(src, 0, 255, cv2.THRESH_OTSU)
-
-cnt, labels, stats, centroids = cv2.connectedComponentsWithStats(src_bin)
-
-dst = cv2.cvtColor(src, cv2.COLOR_GRAY2BGR)
 
 for i in range(1, cnt): # 각각의 객체 정보에 들어가기 위해 반복문. 범위를 1부터 시작한 이유는 배경을 제외
     (x, y, w, h, area) = stats[i]
+    if area < 50: continue
 
-    # 노이즈 제거
-    if area < 20:
-        continue
+    cv2.rectangle(image, (x, y, w, h), 0)
 
-    cv2.rectangle(dst, (x, y, w, h), (0, 255, 255))
-
-cv2.imshow('src', src)
-cv2.imshow('src_bin', src_bin)
-cv2.imshow('dst', dst)
+cv2.imshow('src', image)
+cv2.imshow('src_bin', image_bin)
 cv2.waitKey()
 cv2.destroyAllWindows()
