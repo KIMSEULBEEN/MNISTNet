37c96daf67715a0505dfbbc027a56cf2e57d9008
Seulbeen Kim <tmfqksdk@gmail.com>
2021-02-09 18:02:37 +0900

    CCA + zero padding

--- CCA.py
+++ CCA.py
@@ -1,6 +1,6 @@
 import cv2
 
-image = cv2.imread('sample.jpg', 0)
+image = cv2.imread('sample3.jpg', 0)
 _, width = image.shape
 block_size = width // 5 if (width // 5) % 2 == 1 else width // 5 + 1
 print("block size: ", block_size)
@@ -14,9 +14,11 @@ for i in range(1, cnt):
     (x, y, w, h, area) = stats[i]
     if area < 50: continue
 
-    cv2.rectangle(image, (x, y, w, h), 0)
+    # cv2.rectangle(image, (x, y, w, h), 0)
 
-cv2.imshow('src', image)
-cv2.imshow('src_bin', image_bin)
+image_num = image_bin[y:y+h, x:x+h]
+image_num = cv2.copyMakeBorder(image_num, int(h*0.15), int(h*0.15), int(w*0.12), int(w*0.12), cv2.BORDER_CONSTANT)
+cv2.imwrite("tmp.jpg", image_num)
+
+cv2.imshow("img", image_bin)
 cv2.waitKey()
-cv2.destroyAllWindows()
