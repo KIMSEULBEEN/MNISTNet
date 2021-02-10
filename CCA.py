b89ad91357d9ec11a3da7ab4d1a7f4740c299a8f
Seulbeen Kim <tmfqksdk@gmail.com>
2021-02-11 08:52:17 +0900

    가로/세로 비율  zero padding시 반영

--- CCA.py
+++ CCA.py
@@ -1,7 +1,7 @@
 import cv2
 
-image = cv2.imread('sample3.jpg', 0)
-_, width = image.shape
+image = cv2.imread('img/image3.jpg', 0)
+height, width = image.shape
 block_size = width // 5 if (width // 5) % 2 == 1 else width // 5 + 1
 print("block size: ", block_size)
 
@@ -9,16 +9,24 @@ image_bin = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv
 image_bin = cv2.bitwise_not(image_bin)
 cnt, labels, stats, centroids = cv2.connectedComponentsWithStats(image_bin)
 
-
+x_max, y_max, w_max, h_max, area_max = 0, 0, 0, 0, 0
 for i in range(1, cnt):
     (x, y, w, h, area) = stats[i]
-    if area < 50: continue
+    if (width*height / 2 > area > area_max):
+        x_max, y_max, w_max, h_max, area_max = x, y, w, h, area
 
     # cv2.rectangle(image, (x, y, w, h), 0)
 
-image_num = image_bin[y:y+h, x:x+h]
-image_num = cv2.copyMakeBorder(image_num, int(h*0.15), int(h*0.15), int(w*0.12), int(w*0.12), cv2.BORDER_CONSTANT)
-cv2.imwrite("tmp.jpg", image_num)
 
-cv2.imshow("img", image_bin)
+aspect_ratio = width / height # 가로/세로 비율, 클 수록 가로가 길다
+
+print(aspect_ratio)
+image_num = image_bin[y_max:y_max + h_max, x_max:x_max + w_max]
+image_num = cv2.copyMakeBorder(image_num, int(width * aspect_ratio / 5), int(width * aspect_ratio / 5)
+                               , int(height * aspect_ratio / 5), int(height * aspect_ratio / 5), cv2.BORDER_CONSTANT)
+
+
+cv2.imwrite("img/tmp.jpg", image_num)
+
+cv2.imshow("img", image_num)
 cv2.waitKey()
