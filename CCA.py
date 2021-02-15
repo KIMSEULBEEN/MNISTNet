82a1a10e1062147a2fcb10ad5cf24a71921f58fa
Seulbeen Kim <tmfqksdk@gmail.com>
2021-02-15 15:54:50 +0900

    preprocess_image 함수 추가

--- CCA.py
+++ CCA.py
@@ -1,38 +1,40 @@
 import cv2
 
+def preprocess_image(image):
+    height, width = image.shape
+    block_size = width // 5 if (width // 5) % 2 == 1 else width // 5 + 1
+    print("block size: ", block_size)
+
+    image_bin = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 151, 30)
+    image_bin = cv2.bitwise_not(image_bin)
+    cnt, labels, stats, centroids = cv2.connectedComponentsWithStats(image_bin)
+
+    x_max, y_max, w_max, h_max, area_max = 0, 0, 0, 0, 0
+    for i in range(1, cnt):
+        (x, y, w, h, area) = stats[i]
+        if (width * height / 2 > area > area_max):
+            x_max, y_max, w_max, h_max, area_max = x, y, w, h, area
+
+        # cv2.rectangle(image, (x, y, w, h), 0)
+
+    aspect_ratio = width / height  # 가로/세로 비율, 클 수록 가로가 길다
+
+    print(aspect_ratio)
+    image_num = image_bin[y_max:y_max + h_max, x_max:x_max + w_max]
+    image_num = cv2.copyMakeBorder(image_num, int(width * aspect_ratio / 5), int(width * aspect_ratio / 5)
+                                   , int(height * aspect_ratio / 5), int(height * aspect_ratio / 5), cv2.BORDER_CONSTANT)
+    height, width = image_num.shape
+
+    aspect_num = width - height
+    if (aspect_num > 0):
+        image_num = cv2.copyMakeBorder(image_num, aspect_num // 2, aspect_num // 2, 0, 0, cv2.BORDER_CONSTANT)
+    else:
+        image_num = cv2.copyMakeBorder(image_num, 0, 0, abs(aspect_num // 2), abs(aspect_num // 2), cv2.BORDER_CONSTANT)
+    image_num = cv2.resize(image_num, dsize=(28, 28), interpolation=cv2.INTER_AREA)
+    return image_num
 image = cv2.imread('img/sample4.jpg', 0)
-height, width = image.shape
-block_size = width // 5 if (width // 5) % 2 == 1 else width // 5 + 1
-print("block size: ", block_size)
+image = preprocess_image(image)
 
-image_bin = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 151, 30)
-image_bin = cv2.bitwise_not(image_bin)
-cnt, labels, stats, centroids = cv2.connectedComponentsWithStats(image_bin)
-
-x_max, y_max, w_max, h_max, area_max = 0, 0, 0, 0, 0
-for i in range(1, cnt):
-    (x, y, w, h, area) = stats[i]
-    if (width*height / 2 > area > area_max):
-        x_max, y_max, w_max, h_max, area_max = x, y, w, h, area
-
-    # cv2.rectangle(image, (x, y, w, h), 0)
-
-
-aspect_ratio = width / height # 가로/세로 비율, 클 수록 가로가 길다
-
-print(aspect_ratio)
-image_num = image_bin[y_max:y_max + h_max, x_max:x_max + w_max]
-image_num = cv2.copyMakeBorder(image_num, int(width * aspect_ratio / 5), int(width * aspect_ratio / 5)
-                               , int(height * aspect_ratio / 5), int(height * aspect_ratio / 5), cv2.BORDER_CONSTANT)
-height, width = image_num.shape
-
-aspect_num = width - height
-if (aspect_num > 0):
-    image_num = cv2.copyMakeBorder(image_num, aspect_num // 2, aspect_num // 2, 0, 0, cv2.BORDER_CONSTANT)
-else:
-    image_num = cv2.copyMakeBorder(image_num, 0, 0, abs(aspect_num // 2), abs(aspect_num // 2), cv2.BORDER_CONSTANT)
-image_num = cv2.resize(image_num, dsize=(28, 28), interpolation=cv2.INTER_AREA)
-
-cv2.imwrite("img/tmp.jpg", image_num)
-cv2.imshow("img", image_num)
+cv2.imwrite("img/tmp.jpg", image)
+cv2.imshow("img", image)
 cv2.waitKey()
