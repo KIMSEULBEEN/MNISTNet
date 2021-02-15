60bfbd6defb2dc7507a59732e7ed2d332ea138f3
Seulbeen Kim <tmfqksdk@gmail.com>
2021-02-15 16:34:29 +0900

    코드 간소화(함수화) 진행

--- MNISTNET.py
+++ MNISTNET.py
@@ -3,12 +3,8 @@ import argparse
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
-from torchvision import datasets, transforms
-from torch.optim.lr_scheduler import StepLR
 import cv2
 import numpy as np
-
 
 class Net(nn.Module):
     def __init__(self):
@@ -19,6 +15,8 @@ class Net(nn.Module):
         self.dropout2 = nn.Dropout(0.5)
         self.fc1 = nn.Linear(9216, 128)
         self.fc2 = nn.Linear(128, 10)
+
+        self.load_model()
 
     def forward(self, x):
         x = self.conv1(x)
@@ -35,66 +33,66 @@ class Net(nn.Module):
         output = F.log_softmax(x, dim=1)
         return output
 
-def preprocess_image(image):
-    height, width = image.shape
-    block_size = width // 5 if (width // 5) % 2 == 1 else width // 5 + 1
-    print("block size: ", block_size)
+    def load_model(self):
+        self.load_state_dict(torch.load("mnist_cnn.pt"))
+        self.eval()
 
-    image_bin = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 151, 30)
-    image_bin = cv2.bitwise_not(image_bin)
-    cnt, labels, stats, centroids = cv2.connectedComponentsWithStats(image_bin)
+    def test_image(self):
+        def preprocess_image(image):
+            height, width = image.shape
+            block_size = width // 5 if (width // 5) % 2 == 1 else width // 5 + 1
+            print("block size: ", block_size)
 
-    x_max, y_max, w_max, h_max, area_max = 0, 0, 0, 0, 0
-    for i in range(1, cnt):
-        (x, y, w, h, area) = stats[i]
-        if (width * height / 2 > area > area_max):
-            x_max, y_max, w_max, h_max, area_max = x, y, w, h, area
+            image_bin = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 151, 30)
+            image_bin = cv2.bitwise_not(image_bin)
+            cnt, labels, stats, centroids = cv2.connectedComponentsWithStats(image_bin)
 
-        # cv2.rectangle(image, (x, y, w, h), 0)
+            x_max, y_max, w_max, h_max, area_max = 0, 0, 0, 0, 0
+            for i in range(1, cnt):
+                (x, y, w, h, area) = stats[i]
+                if (width * height / 2 > area > area_max):
+                    x_max, y_max, w_max, h_max, area_max = x, y, w, h, area
 
-    aspect_ratio = width / height  # 가로/세로 비율, 클 수록 가로가 길다
+                # cv2.rectangle(image, (x, y, w, h), 0)
 
-    print(aspect_ratio)
-    image_num = image_bin[y_max:y_max + h_max, x_max:x_max + w_max]
-    image_num = cv2.copyMakeBorder(image_num, int(width * aspect_ratio / 5), int(width * aspect_ratio / 5)
-                                   , int(height * aspect_ratio / 5), int(height * aspect_ratio / 5), cv2.BORDER_CONSTANT)
-    height, width = image_num.shape
+            aspect_ratio = width / height  # 가로/세로 비율, 클 수록 가로가 길다
 
-    aspect_num = width - height
-    if (aspect_num > 0):
-        image_num = cv2.copyMakeBorder(image_num, aspect_num // 2, aspect_num // 2, 0, 0, cv2.BORDER_CONSTANT)
-    else:
-        image_num = cv2.copyMakeBorder(image_num, 0, 0, abs(aspect_num // 2), abs(aspect_num // 2), cv2.BORDER_CONSTANT)
-    image_num = cv2.resize(image_num, dsize=(28, 28), interpolation=cv2.INTER_AREA)
-    return image_num
+            print(aspect_ratio)
+            image_num = image_bin[y_max:y_max + h_max, x_max:x_max + w_max]
+            image_num = cv2.copyMakeBorder(image_num, int(width * aspect_ratio / 5), int(width * aspect_ratio / 5)
+                                           , int(height * aspect_ratio / 5), int(height * aspect_ratio / 5), cv2.BORDER_CONSTANT)
+            height, width = image_num.shape
 
-
-def load_test(model):
-    img_tensor = np.zeros([1, 1, 28, 28], dtype=np.double)
-    image = cv2.imread('img/sample4.jpg', 0)
-    image = preprocess_image(image)
-    img_tensor[0, 0, :, :] = image
-    t = torch.from_numpy(img_tensor).float()
-    pred = (int)(model(t).argmax(dim=1, keepdim=True)[0][0])
-    print(pred)
-
+            aspect_num = width - height
+            if (aspect_num > 0):
+                image_num = cv2.copyMakeBorder(image_num, aspect_num // 2, aspect_num // 2, 0, 0, cv2.BORDER_CONSTANT)
+            else:
+                image_num = cv2.copyMakeBorder(image_num, 0, 0, abs(aspect_num // 2), abs(aspect_num // 2), cv2.BORDER_CONSTANT)
+            image_num = cv2.resize(image_num, dsize=(28, 28), interpolation=cv2.INTER_AREA)
+            return image_num
+        img_tensor = np.zeros([1, 1, 28, 28], dtype=np.double)
+        image = cv2.imread('img/sample4.jpg', 0)
+        image = preprocess_image(image)
+        img_tensor[0, 0, :, :] = image
+        t = torch.from_numpy(img_tensor).float()
+        pred = (int)(self(t).argmax(dim=1, keepdim=True)[0][0])
+        print(pred)
 
 def main():
-    # Training settings
-    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
-    parser.add_argument('--no-cuda', action='store_true', default=True, help='disables CUDA training')
-    parser.add_argument('--seed', type=int, default=1, metavar='S', help='random seed (default: 1)')
-    args = parser.parse_args()
+    def set_args():
+        # Training settings
+        parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+        parser.add_argument('--no-cuda', action='store_true', default=True, help='disables CUDA training')
+        parser.add_argument('--seed', type=int, default=1, metavar='S', help='random seed (default: 1)')
+        args = parser.parse_args()
 
-    use_cuda = not args.no_cuda and torch.cuda.is_available()
-    torch.manual_seed(args.seed)
-    device = torch.device("cuda" if use_cuda else "cpu")
-    model = Net().to(device)
-    model.load_state_dict(torch.load("mnist_cnn.pt"))
-    model.eval()
+        use_cuda = not args.no_cuda and torch.cuda.is_available()
+        torch.manual_seed(args.seed)
+        device = torch.device("cuda" if use_cuda else "cpu")
+        return device
 
-
-    load_test(model)
+    model = Net().to(set_args())
+    model.test_image()
 
 
 if __name__ == '__main__':
